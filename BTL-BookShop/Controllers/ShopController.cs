87b143c746a58e77f09b8c0cb7f1f0f8da356aea
lanhuong <lanhuong@DESKTOP-EGER9U2>
2019-10-18 08:07:49 +0700

    Add project files.

---
+++ BTL-BookShop/Controllers/ShopController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BTL_BookShop.Controllers
+{
+    public class ShopController : Controller
+    {
+        // GET: Shop
+        public ActionResult Category()
+        {
+            return View();
+        }
+
+        public ActionResult Product()
+        {
+            return View();
+        }
+        public ActionResult Search()
+        {
+            return View();
+        }
+    }
+}
---
+++ BTL-BookShop/Controllers/UserController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BTL_BookShop.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult ViewCart()
+        {
+            return View();
+        }
+
+        public ActionResult CheckOut()
+        {
+            return View();
+        }
+       
+    }
+}
