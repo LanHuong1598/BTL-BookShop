49537d60a319bb184a30d168828540d6ba4dd06a
khanhtoan99gvc <khanhtoan99gvc@gmail.com>
2019-10-18 08:35:51 +0700

    entities framework

---
+++ BTL-BookShop/Models/Function/F_User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using BTL_BookShop.Models.Entities;
+namespace BTL_BookShop.Models.Function
+{
+    public class F_User
+    {
+        private MyDBContext context;
+        public F_User()
+        {
+            context = new MyDBContext();
+        }
+    }
+}
