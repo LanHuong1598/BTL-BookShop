997d361f55129cf551f245be084b06e991f9908d
huyhoang189 <vohuyhoang189@gmail.com>
2019-10-18 09:22:38 +0700

    udate

---
+++ BTL-BookShop/Models/Function/F_Book.cs
@@ -0,0 +1,24 @@
+﻿using BTL_BookShop.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BTL_BookShop.Models.Function
+{
+   
+    public class F_Book
+    {
+        MyDBContext context = null;
+        public F_Book()
+        {
+            context = new MyDBContext();
+        }
+        public List<Book> getAll()
+        {
+            List<Book> ans = context.Books.ToList();
+            return ans;
+        }
+
+    }
+}
