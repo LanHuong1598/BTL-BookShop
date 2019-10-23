256b9a69393c360b67d3308bad5f62ce3001c86e
soncutemta1234 <ngocsonqh1@gmail.com>
2019-10-23 08:43:53 +0700

    change

---
+++ BTL-BookShop/Models/Functions/F_Book.cs
@@ -0,0 +1,73 @@
+﻿using BTL_BookShop.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BTL_BookShop.Models.Functions
+{
+    public class F_Book
+    {
+        private MyDBContext content;
+        public F_Book()
+        {
+            content = new MyDBContext();
+        }
+        public IQueryable<Book> ListBooks
+        {
+            get { return content.Books; }
+        }
+        public Book FindBook(long ma)
+        {
+            Book temp = content.Books.Find(ma);
+            return temp;
+        }
+        public long? Insert(Book model)
+        {
+            Book temp = content.Books.Find(model.ID);
+            if (temp == null)
+            {
+                return null;
+            }
+            else
+            {
+                content.Books.Add(model);
+                content.SaveChanges();
+                return model.ID;
+            }
+        }
+        public long? Update(Book model)
+        {
+            Book temp = content.Books.Find(model.ID);
+            if (temp == null)
+            {
+                return null;
+            }
+            else
+            {
+                temp = model;
+                content.SaveChanges();
+                return model.ID;
+            }
+        }
+        public long? Delete(Book model)
+        {
+            Book temp = content.Books.Find(model.ID);
+            if (temp == null)
+            {
+                return null;
+            }
+            else
+            {
+                content.Books.Remove(model);
+                content.SaveChanges();
+                return model.ID;
+            }
+        }
+        public List<Book> getAll()
+        {
+            List<Book> ans = content.Books.ToList();
+            return ans;
+        }
+    }
+}
