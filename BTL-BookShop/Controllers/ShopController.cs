62adeb8e8a9897bdbbc2a88e4c8f604101578072
lanhuong <lanhuong@DESKTOP-EGER9U2>
2019-10-31 20:48:15 +0700

    2.

--- BTL-BookShop/Controllers/ShopController.cs
+++ BTL-BookShop/Controllers/ShopController.cs
@@ -1,4 +1,5 @@
-﻿using BTL_BookShop.Models.Function;
+﻿using BTL_BookShop.Models.Entities;
+using BTL_BookShop.Models.Function;
 using BTL_BookShop.Models.Functions;
 using System;
 using System.Collections.Generic;
@@ -33,6 +34,44 @@ namespace BTL_BookShop.Controllers
             
             return View();
         }
+        public ActionResult timkiemtheocategory(long id)
+        {
+            var listBook = new F_CategoryBook().getAll().Where(x => x.IDCategory == id).ToList();
+            var dsBook = new F_Book().getAll();
+            var list = (from book in listBook
+                        join book1 in dsBook on book.IDBook equals book1.ID
+                        select new Book
+                        {
+                            ID = book.ID,
+                            Name = book1.Name,
+                            Image = book1.Image,
+                            Price = book1.Price
 
+                        });
+            ViewBag.Book = list;
+            F_Category fctg = new F_Category();
+            ViewBag.ListCategory = fctg.getAll();
+
+            return View("Search");
+        }
+
+        public ActionResult timkiemtheoNXB(long id)
+        {
+
+            var dsBook = new F_Book().getAll().Where(x => x.Publisher == id);
+            ViewBag.Book = dsBook;
+            F_Category fctg = new F_Category();
+            ViewBag.ListCategory = fctg.getAll();
+            return View("Search");
+        }
+        public ActionResult timkiemtheotacgia(long id)
+        {
+
+            var dsBook = new F_Book().getAll().Where(x => x.Author == id);
+            ViewBag.Book = dsBook;
+            F_Category fctg = new F_Category();
+            ViewBag.ListCategory = fctg.getAll();
+            return View("Search");
+        }
     }
 }
