75158423bd678c0669484a617300b91c4a8c3c8a
lanhuong <lanhuong@DESKTOP-EGER9U2>
2019-11-01 09:11:50 +0700

    4

--- BTL-BookShop/Controllers/ShopController.cs
+++ BTL-BookShop/Controllers/ShopController.cs
@@ -6,7 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using PagedList;
 namespace BTL_BookShop.Controllers
 {
     public class ShopController : Controller
@@ -25,10 +25,30 @@ namespace BTL_BookShop.Controllers
             ViewBag.Book = model;
             return View();
         }
-        public ActionResult Search(string txt)
+        public ActionResult Search(string txt, int? page)
         {
-            var model = new F_Book().getAll().Where(x => x.Name.Contains(txt)).ToList();
-            ViewBag.Book = model;
+            if (txt == null) txt = "";
+            // 1. Tham số int? dùng để thể hiện null và kiểu int
+            // page có thể có giá trị là null và kiểu int.
+
+            // 2. Nếu page = null thì đặt lại là 1.
+            if (page == null) page = 1;
+
+            // 3. Tạo truy vấn, lưu ý phải sắp xếp theo trường nào đó, ví dụ OrderBy
+            // theo LinkID mới có thể phân trang.
+            var model = new F_Book().getAll().Where(x => x.Name.Contains(txt)).OrderBy(x => x.ID).ToList();
+
+            // 4. Tạo kích thước trang (pageSize) hay là số Link hiển thị trên 1 trang
+            int pageSize = 3;
+
+            // 4.1 Toán tử ?? trong C# mô tả nếu page khác null thì lấy giá trị page, còn
+            // nếu page = null thì lấy giá trị 1 cho biến pageNumber.
+            int pageNumber = (page ?? 1);
+
+            // 5. Trả về các Link được phân trang theo kích thước và số trang.
+            
+
+            ViewBag.Book = model.ToPagedList(pageNumber, pageSize);
             F_Category fctg = new F_Category();
             ViewBag.ListCategory = fctg.getAll();
             
@@ -68,6 +88,16 @@ namespace BTL_BookShop.Controllers
         {
 
             var dsBook = new F_Book().getAll().Where(x => x.Author == id);
+            ViewBag.Book = dsBook;
+            F_Category fctg = new F_Category();
+            ViewBag.ListCategory = fctg.getAll();
+            return View("Search");
+        }
+        public ActionResult timkiemtheogia(string begin, string end)
+        {
+            int u = Convert.ToInt32(begin);
+            int v = Convert.ToInt32(end);
+            var dsBook = new F_Book().getAll().Where(x => x.Price >= u && x.Price <= v);
             ViewBag.Book = dsBook;
             F_Category fctg = new F_Category();
             ViewBag.ListCategory = fctg.getAll();
